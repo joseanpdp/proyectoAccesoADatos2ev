e7ced06601b3f47af33adb9eb98bf1c0659441d1
jose <joseanperezdp@gmail.com>
2024-02-15 13:45:22 +0100

    Completada realización del proyecto de Acceso a Datos sin Flutter

---
+++ src/main/java/jajm/apitwitter/security/authentication/AuthenticationController.java
@@ -0,0 +1,48 @@
+package jajm.apitwitter.security.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jajm.apitwitter.security.authentication.dto.AuthenticationResponseDTO;
+import jajm.apitwitter.security.authentication.dto.LoginRequestDTO;
+import jajm.apitwitter.security.authentication.dto.RegisterRequestDTO;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Controlador que maneja las operaciones relacionadas con la autenticación.
+ */
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+	@Autowired
+	private final AuthenticationService authentificationService;
+
+	/**
+	 * Maneja la solicitud de inicio de sesión.
+	 *
+	 * @param request DTO que contiene la información de inicio de sesión.
+	 * @return Respuesta con el token de autenticación generado.
+	 */
+	@PostMapping("login")
+	public ResponseEntity<AuthenticationResponseDTO> login(@RequestBody LoginRequestDTO request) {
+		return ResponseEntity.ok(authentificationService.login(request));
+	}
+
+	/**
+	 * Maneja la solicitud de registro de usuario.
+	 *
+	 * @param request DTO que contiene la información de registro.
+	 * @return Respuesta con el token de autenticación generado.
+	 */
+	@PostMapping("register")
+	public ResponseEntity<AuthenticationResponseDTO> register(@RequestBody RegisterRequestDTO request) {
+		return ResponseEntity.ok(authentificationService.register(request));
+	}
+
+}
